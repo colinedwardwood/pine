6815fa9f2227e52def67e3ff7389e4f8e32b22d4
cewdesign <woodce@gmail.com>
2018-11-17 17:36:30 -0500

    1298 million seconds exercise added

--- timecalcs.rb
+++ timecalcs.rb
@@ -10,3 +10,19 @@ print 'Age in seconds: '
 today = Date.new(2018,11,17)
 birthday = Date.new(1978,02,07)
 puts (today - birthday).to_i * 24 * 60 * 60
+
+puts 'I am 1298 million seconds old, how old am I?'
+puts '1298 million seconds is: '
+
+print 1298000000 / 60
+puts ' Minutes'
+
+print 1298000000 / 60 / 60
+puts ' Hours'
+
+print 1298000000 / 60 / 60 / 24
+puts ' Days'
+
+print 1298000000 / 60 / 60 / 24 / 365
+puts ' Years'
+
