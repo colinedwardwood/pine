cea0d571a5f379ab73e11be959fc8a49abccf30e
cewdesign <woodce@gmail.com>
2018-11-17 17:23:50 -0500

    added exercises for minutes/year, etc.

--- timecalcs.rb
+++ timecalcs.rb
@@ -1,2 +1,12 @@
+require 'date'
+
 print 'Hours in a year: '
 puts 24 * 365
+
+print 'Minutes in decade: '
+puts 10 * 365 * 24 * 60
+
+print 'Age in seconds: '
+today = Date.new(2018,11,17)
+birthday = Date.new(1978,02,07)
+puts (today - birthday).to_i * 24 * 60 * 60
